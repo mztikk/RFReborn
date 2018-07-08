c53bff058d7a95b86cf306929e6caa90753e6f7a
mztikk <nevigskcuforez@gmail.com>
2018-07-08 16:12:10 +0200

    added hashing

---
+++ RFReborn/Hashing.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Hash
+{
+	/// <summary>
+	/// Provides various hashing functionalities.
+	/// </summary>
+	public static class Hashing
+	{
+		/// <summary>
+		/// List of all valid hash names
+		/// </summary>
+		public static ReadOnlyCollection<string> ValidHashes = new ReadOnlyCollection<string>(new List<string>() { "MD5", "SHA1", "SHA256", "SHA384", "SHA512" });
+
+		/// <summary>
+		/// Gets the name of the hash as represented in <see cref="ValidHashes"/>. Turns "md5" into "MD5" for example.
+		/// </summary>
+		/// <param name="name">Name of the hash to get the actual key</param>
+		/// <returns>Returns the actual name of the hash or <see cref="string.Empty"/> if it does not exist.</returns>
+		public static string GetHashName(string name)
+		{
+			foreach (var hash in ValidHashes)
+			{
+				if (hash.Equals(name, StringComparison.OrdinalIgnoreCase))
+				{
+					return hash;
+				}
+			}
+
+			return string.Empty;
+		}
+
+		/// <summary>
+		/// Hashes the stream with the given hash algorithm, uses <see cref="Encoding.UTF8"/> to encode the input string to bytes.
+		/// </summary>
+		/// <param name="hashName">Hash algorithm to use</param>
+		/// <param name="input">Input to hash</param>
+		/// <returns>Hex string of the hashed input</returns>
+		public static string Hash(string hashName, string input) => Hash(hashName, input, Encoding.UTF8);
+
+		/// <summary>
+		/// Hashes the stream with the given hash algorithm.
+		/// </summary>
+		/// <param name="hashName">Hash algorithm to use</param>
+		/// <param name="input">Input to hash</param>
+		/// <param name="encoding">Encoding to use to encode the input string to bytes</param>
+		/// <returns>Hex string of the hashed input</returns>
+		public static string Hash(string hashName, string input, Encoding encoding) => Hash(hashName, encoding.GetBytes(input));
+
+		/// <summary>
+		/// Hashes the stream with the given hash algorithm.
+		/// </summary>
+		/// <param name="hashName">Hash algorithm to use</param>
+		/// <param name="input">Input to hash</param>
+		/// <returns>Hex string of the hashed input</returns>
+		public static string Hash(string hashName, byte[] input)
+		{
+			if (!ValidHashes.Contains(hashName))
+			{
+				throw new ArgumentException(nameof(hashName), "Hash does not exist");
+			}
+
+			using (var hashAlgorithm = (HashAlgorithm)CryptoConfig.CreateFromName(hashName))
+			{
+				var hashedBytes = hashAlgorithm.ComputeHash(input);
+				return Stringify(hashedBytes);
+			}
+		}
+
+		/// <summary>
+		/// Hashes the stream with the given hash algorithm.
+		/// </summary>
+		/// <param name="hashName">Hash algorithm to use</param>
+		/// <param name="input">Input to hash</param>
+		/// <returns>Hex string of the hashed input</returns>
+		public static string Hash(string hashName, Stream input)
+		{
+			if (!ValidHashes.Contains(hashName))
+			{
+				throw new ArgumentException(nameof(hashName), "Hash does not exist");
+			}
+
+			using (var hashAlgorithm = (HashAlgorithm)CryptoConfig.CreateFromName(hashName))
+			{
+				var hashedBytes = hashAlgorithm.ComputeHash(input);
+				return Stringify(hashedBytes);
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static string Stringify(byte[] hexBytes)
+		{
+			var rtn = new StringBuilder();
+			for (var i = 0; i < hexBytes.Length; i++)
+			{
+				rtn.Append(hexBytes[i].ToString("X2"));
+			}
+			return rtn.ToString();
+		}
+	}
+}
---
+++ RFReborn/StringR.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RFReborn
+{
+	/// <summary>
+	/// Provides functionality to manipulate and operate on <see cref="string"/>
+	/// </summary>
+	public static class StringR
+	{
+		private static readonly HashSet<char> _WhitespaceChars = new HashSet<char>
+		{
+			'\u0020',
+			'\u00A0',
+			'\u1680',
+			'\u2000',
+			'\u2001',
+			'\u2002',
+			'\u2003',
+			'\u2004',
+			'\u2005',
+			'\u2006',
+			'\u2007',
+			'\u2008',
+			'\u2009',
+			'\u200A',
+			'\u202F',
+			'\u205F',
+			'\u3000',
+			'\u2028',
+			'\u2029',
+			'\u0009',
+			'\u000A',
+			'\u000B',
+			'\u000C',
+			'\u000D',
+			'\u0085'
+		};
+
+		/// <summary>
+		/// Removes all whitespace chars from a string and returns it.
+		/// </summary>
+		/// <param name="input">String to remove whitespace from</param>
+		/// <returns>A new string without whitespace</returns>
+		public static string RemoveWhitespace(string input) => RemoveChars(input, _WhitespaceChars);
+
+		/// <summary>
+		/// Removes the specified chars from the string and returns it.
+		/// </summary>
+		/// <param name="input">String where the chars should be removed from</param>
+		/// <param name="chars">The chars to remove</param>
+		/// <returns>A new string, without the chars</returns>
+		public static string RemoveChars(string input, ICollection<char> chars)
+		{
+			var len = input.Length;
+			var src = input.ToCharArray();
+			var dstIdx = 0;
+			for (var i = 0; i < len; i++)
+			{
+				var ch = src[i];
+				if (chars.Contains(ch))
+				{
+					continue;
+				}
+
+				src[dstIdx++] = ch;
+			}
+
+			return new string(src, 0, dstIdx);
+		}
+	}
+}
