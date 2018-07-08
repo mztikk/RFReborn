693e3f93fe2b9b217a120838fbea82e94a1f9210
mztikk <nevigskcuforez@gmail.com>
2018-07-08 16:16:02 +0200

    hashing namespace fix

--- RFReborn/Hashing.cs
+++ RFReborn/Hashing.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace Hash
+namespace RFReborn
 {
 	/// <summary>
 	/// Provides various hashing functionalities.
