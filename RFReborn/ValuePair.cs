b84b6bcba5fc937419ee382c00be09060b4c09c3
mztikk <nevigskcuforez@gmail.com>
2019-08-08 20:04:58 +0200

    added StructPair<T> and ValuePair<T>

---
+++ RFReborn/ValuePair.cs
@@ -0,0 +1,30 @@
+﻿namespace RFReborn
+{
+    /// <summary>
+    /// Class that holds two values as a pair.
+    /// </summary>
+    /// <typeparam name="T">type of value</typeparam>
+    public class ValuePair<T>
+    {
+        /// <summary>
+        /// Left Value of the ValuePair
+        /// </summary>
+        public T Left;
+
+        /// <summary>
+        /// Right Value of the ValuePair
+        /// </summary>
+        public T Right;
+
+        /// <summary>
+        /// Creates a new <see cref="ValuePair{T}"/> instance
+        /// </summary>
+        /// <param name="left">left value</param>
+        /// <param name="right">right value</param>
+        public ValuePair(T left, T right)
+        {
+            Left = left;
+            Right = right;
+        }
+    }
+}
