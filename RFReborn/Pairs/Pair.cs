5b0b589cd4c547aea46307416c47b48233c5da4f
mztikk <nevigskcuforez@gmail.com>
2019-08-17 21:12:44 +0200

    refactored ICollection Extensions for adding Pair and ValuePair to their respective class

--- RFReborn/Pairs/Pair.cs
+++ RFReborn/Pairs/Pair.cs
@@ -1,4 +1,6 @@
-﻿namespace RFReborn.Pairs
+﻿using System.Collections.Generic;
+
+namespace RFReborn.Pairs
 {
     /// <summary>
     /// Class that holds two values as a pair.
@@ -52,6 +54,24 @@
         public static implicit operator Pair<T>(RefValuePair<T> refValuePair)
         {
             return new Pair<T>(refValuePair.Left, refValuePair.Right);
+        }
+    }
+
+    /// <summary>
+    /// Extends <see cref="ICollection{T}"/>.
+    /// </summary>
+    public static class PairCollection
+    {
+        /// <summary>
+        /// Adds a new <see cref="Pair{T}"/> to the <paramref name="collection"/>
+        /// </summary>
+        /// <typeparam name="T">type of <see cref="Pair{T}"/></typeparam>
+        /// <param name="collection"><see cref="ICollection{T}"/> to add to</param>
+        /// <param name="left">left value</param>
+        /// <param name="right">right value</param>
+        public static void Add<T>(this ICollection<Pair<T>> collection, T left, T right)
+        {
+            collection.Add(new Pair<T>(left, right));
         }
     }
 }
--- RFReborn/Pairs/ValuePair.cs
+++ RFReborn/Pairs/ValuePair.cs
@@ -1,4 +1,6 @@
-﻿namespace RFReborn.Pairs
+﻿using System.Collections.Generic;
+
+namespace RFReborn.Pairs
 {
     /// <summary>
     /// Struct that holds two values as a pair.
@@ -52,6 +54,24 @@
         public static implicit operator ValuePair<T>(RefValuePair<T> refValuePair)
         {
             return new ValuePair<T>(refValuePair.Left, refValuePair.Right);
+        }
+    }
+
+    /// <summary>
+    /// Extends <see cref="ICollection{T}"/>.
+    /// </summary>
+    public static class ValuePairCollection
+    {
+        /// <summary>
+        /// Adds a new <see cref="ValuePair{T}"/> to the <paramref name="collection"/>
+        /// </summary>
+        /// <typeparam name="T">type of <see cref="ValuePair{T}"/></typeparam>
+        /// <param name="collection"><see cref="ICollection{T}"/> to add to</param>
+        /// <param name="left">left value</param>
+        /// <param name="right">right value</param>
+        public static void Add<T>(this ICollection<ValuePair<T>> collection, T left, T right)
+        {
+            collection.Add(new ValuePair<T>(left, right));
         }
     }
 }
