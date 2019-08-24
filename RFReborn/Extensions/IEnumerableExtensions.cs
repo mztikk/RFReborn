ae5f2e0afd579d9fc79d8ce007154722a27102af
mztikk <nevigskcuforez@gmail.com>
2019-08-24 12:17:05 +0200

    added int Count<T>(this IEnumerable<T>), better performance of Any<T>(this IEnumerable<T>)

--- RFReborn/Extensions/IEnumerableExtensions.cs
+++ RFReborn/Extensions/IEnumerableExtensions.cs
@@ -24,12 +24,41 @@ namespace RFReborn.Extensions
         /// <returns>Returns TRUE if it contains any item; FALSE otherwise.</returns>
         public static bool Any<T>(this IEnumerable<T> ienum)
         {
+            if (ienum is ICollection<T> icoll)
+            {
+                return icoll.Count > 0;
+            }
+
             foreach (T item in ienum)
             {
                 return true;
             }
 
             return false;
+        }
+
+        /// <summary>
+        /// Returns the number of elements in <paramref name="ienum"/>.
+        /// Tries to cast to <see cref="ICollection{T}"/> to get access to the Count property which should be its size,
+        /// otherwise it loops through the <see cref="IEnumerable{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of <see cref="IEnumerable{T}"/></typeparam>
+        /// <param name="ienum"><see cref="IEnumerable{T}"/> to get Size of</param>
+        /// <returns>Returns the number of elements in <paramref name="ienum"/>.</returns>
+        public static int Count<T>(this IEnumerable<T> ienum)
+        {
+            if (ienum is ICollection<T> icoll)
+            {
+                return icoll.Count;
+            }
+
+            int count = 0;
+            foreach (T item in ienum)
+            {
+                count++;
+            }
+
+            return count;
         }
 
         /// <summary>
