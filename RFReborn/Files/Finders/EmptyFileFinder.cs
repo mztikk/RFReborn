cc1e56a076de923c13599665bb3a2b25a75d982a
mztikk <nevigskcuforez@gmail.com>
2019-09-14 10:17:56 +0200

    added EmptyFileFinder

---
+++ RFReborn/Files/Finders/EmptyFileFinder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RFReborn.Files.Finders
+{
+    /// <summary>
+    /// Provides methods to find empty files
+    /// </summary>
+    public class EmptyFileFinder : IFileFinder
+    {
+        /// <summary>
+        /// Walks the <paramref name="root"/> path and finds all empty files.
+        /// </summary>
+        /// <param name="root">Path where to start walking</param>
+        /// <returns><see cref="IEnumerable{T}"/> of empty <see cref="FileInfo"/></returns>
+        public IEnumerable<FileInfo> Find(string root)
+        {
+            HashSet<FileInfo> emptyFiles = new HashSet<FileInfo>();
+
+            FileUtils.Walk(root, (FileInfo fi) =>
+            {
+                try
+                {
+                    if (fi.Length == 0)
+                    {
+                        emptyFiles.Add(fi);
+                    }
+                }
+                catch (IOException) { }
+                catch (UnauthorizedAccessException) { }
+            });
+
+            return emptyFiles;
+        }
+    }
+}
