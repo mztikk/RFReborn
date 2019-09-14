43f53723e335e1845306d0e4d67693c83f4e9ba6
mztikk <nevigskcuforez@gmail.com>
2019-09-14 10:16:48 +0200

    added IFileFinder

---
+++ RFReborn/Files/Finders/IFileFinder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace RFReborn.Files.Finders
+{
+    /// <summary>
+    /// Defines methods to find files
+    /// </summary>
+    public interface IFileFinder
+    {
+        /// <summary>
+        /// Finds files in the <paramref name="root"/> path.
+        /// </summary>
+        /// <param name="root">Path where to start walking</param>
+        /// <returns><see cref="IEnumerable{T}"/> of <see cref="FileInfo"/></returns>
+        IEnumerable<FileInfo> Find(string root);
+    }
+}
