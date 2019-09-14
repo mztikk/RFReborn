1e82a1c7d4b6c79c681b29fd944d77977524f5c8
mztikk <nevigskcuforez@gmail.com>
2019-09-14 15:07:07 +0200

    added DuplicateFinder test

---
+++ RFReborn/Files/DuplicateFinder.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using RFReborn.Hashing;
+
+namespace RFReborn.Files
+{
+    public class DuplicateFinder
+    {
+        public delegate string HASH(FileInfo fileInfo);
+
+        public DuplicateFinder(HASH hashFunc) => HashFunc = hashFunc;
+
+        public DuplicateFinder() : this(XXHASH) { }
+
+        public HASH HashFunc { get; set; }
+
+        public Dictionary<string, List<FileInfo>> Find(string root) => Find(new string[] { root });
+
+        public Dictionary<string, List<FileInfo>> Find(string[] rootPaths)
+        {
+            Dictionary<string, FileInfo> cache = new Dictionary<string, FileInfo>();
+            Dictionary<string, List<FileInfo>> dupes = new Dictionary<string, List<FileInfo>>();
+
+            foreach (string root in rootPaths)
+            {
+                FileUtils.Walk(root, (FileInfo fi) =>
+                {
+                    try
+                    {
+                        string hash = HashFunc(fi);
+                        if (cache.ContainsKey(hash))
+                        {
+                            if (dupes.ContainsKey(hash))
+                            {
+                                dupes[hash].Add(fi);
+                            }
+                            else
+                            {
+                                dupes.TryAdd(hash, new List<FileInfo> { cache[hash], fi });
+                            }
+                        }
+                        else
+                        {
+                            cache.TryAdd(hash, fi);
+                        }
+                    }
+                    catch (IOException) { }
+                    catch (UnauthorizedAccessException) { }
+
+                    return true;
+                });
+            }
+
+            return dupes;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static string SHA512HASH(FileInfo input) => HashFactory.Hash("SHA512", input);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static unsafe string XXHASH(FileInfo input)
+        {
+            using (FileStream fRead = input.OpenRead())
+            {
+                byte[] buffer = new byte[fRead.Length];
+                fRead.Read(buffer);
+                return xxHash.Hash(buffer).ToString();
+            }
+        }
+    }
+}
