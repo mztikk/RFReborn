717232bcee2698936b92457f47785cf3c134cb36
mztikk <nevigskcuforez@gmail.com>
2019-09-14 14:04:03 +0200

    added IDirectoryFinder

---
+++ RFReborn/Files/Finders/IDirectoryFinder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace RFReborn.Files.Finders
+{
+    /// <summary>
+    /// Defines methods to find directories
+    /// </summary>
+    public interface IDirectoryFinder
+    {
+        /// <summary>
+        /// Finds directories in the <paramref name="root"/> path.
+        /// </summary>
+        /// <param name="root">Path where to start walking</param>
+        /// <returns><see cref="IEnumerable{T}"/> of <see cref="DirectoryInfo"/></returns>
+        IEnumerable<DirectoryInfo> Find(string root);
+    }
+}
