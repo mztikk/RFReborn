2fcf440fc73ecc5d97bb4fce856209d38b3ba5be
mztikk <nevigskcuforez@gmail.com>
2019-09-14 14:06:36 +0200

    added EmptyDirectoryFinder

---
+++ RFReborn/Files/Finders/EmptyDirectoryFinder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace RFReborn.Files.Finders
+{
+    /// <summary>
+    /// Provides methods to find empty directories
+    /// </summary>
+    public class EmptyDirectoryFinder : IDirectoryFinder
+    {
+        /// <summary>
+        /// <para>Walks the <paramref name="root"/> path and finds all empty directories.</para>
+        /// <para>If a directory contains empty directores only the top level empty one will be returned.</para>
+        /// </summary>
+        /// <param name="root">Path where to start walking</param>
+        /// <returns><see cref="IEnumerable{T}"/> of empty <see cref="DirectoryInfo"/></returns>
+        public IEnumerable<DirectoryInfo> Find(string root)
+        {
+            HashSet<DirectoryInfo> emptyDirs = new HashSet<DirectoryInfo>();
+
+            FileUtils.Walk(root, (DirectoryInfo di) =>
+            {
+                (ulong fileCount, ulong folderCount) = FileUtils.CountFilesAndFolders(di.FullName);
+                if (fileCount == 0)
+                {
+                    emptyDirs.Add(di);
+
+                    if (folderCount > 0)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            });
+
+            return emptyDirs;
+        }
+    }
+}
