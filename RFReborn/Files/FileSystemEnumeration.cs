af0512254a0f7101c7b191d9c2209be35db2ebde
mztikk <nevigskcuforez@gmail.com>
2019-09-16 17:59:47 +0200

    added FileSystemEnumeration and FileUtils IEnumerable<string> Walk(string, FileSystemEnumeration) to enumerate the tree

---
+++ RFReborn/Files/FileSystemEnumeration.cs
@@ -0,0 +1,23 @@
+﻿namespace RFReborn.Files
+{
+    /// <summary>
+    /// Options to enumerate the FileSystem
+    /// </summary>
+    public enum FileSystemEnumeration : byte
+    {
+        /// <summary>
+        /// Enumerate only Files
+        /// </summary>
+        FilesOnly = 0,
+
+        /// <summary>
+        /// Enumerate only Directories
+        /// </summary>
+        DirectoriesOnly = 1,
+
+        /// <summary>
+        /// Enumerate Files and Directories
+        /// </summary>
+        FilesAndDirectories = 2
+    }
+}
--- RFReborn/Files/FileUtils.cs
+++ RFReborn/Files/FileUtils.cs
@@ -291,5 +291,97 @@ namespace RFReborn.Files
                 }
             }
         }
+
+        /// <summary>
+        /// Enumerates the tree of a root path, returning files and directories
+        /// </summary>
+        /// <param name="root">Path where to start walking</param>
+        /// <returns></returns>
+        public static IEnumerable<string> Walk(string root) => Walk(root, FileSystemEnumeration.FilesAndDirectories);
+
+        /// <summary>
+        /// Enumerates the tree of a root path
+        /// </summary>
+        /// <param name="root">Path where to start walking</param>
+        /// <param name="fileSystemEnumeration">Whether to return only files, directories or both</param>
+        /// <returns></returns>
+        public static IEnumerable<string> Walk(string root, FileSystemEnumeration fileSystemEnumeration)
+        {
+            // Data structure to hold names of subfolders to be
+            // examined for files.
+            Stack<string> dirs = new Stack<string>(InitialCapacity);
+            if (!Directory.Exists(root))
+            {
+                throw new ArgumentException();
+            }
+
+            bool enumFiles = fileSystemEnumeration == FileSystemEnumeration.FilesOnly || fileSystemEnumeration == FileSystemEnumeration.FilesAndDirectories;
+            bool enumDirs = fileSystemEnumeration == FileSystemEnumeration.DirectoriesOnly || fileSystemEnumeration == FileSystemEnumeration.DirectoriesOnly;
+
+            dirs.Push(root);
+            while (dirs.Count > 0)
+            {
+                string currentDir = dirs.Pop();
+
+                if (enumDirs)
+                {
+                    yield return currentDir;
+
+                }
+
+                IEnumerable<string> subDirs;
+                try
+                {
+                    subDirs = Directory.EnumerateDirectories(currentDir);
+                }
+                // An UnauthorizedAccessException exception will be thrown if we do not have
+                // discovery permission on a folder or file. It may or may not be acceptable
+                // to ignore the exception and continue enumerating the remaining files and
+                // folders. It is also possible (but unlikely) that a DirectoryNotFound exception
+                // will be raised. This will happen if currentDir has been deleted by
+                // another application or thread after our call to Directory.Exists. The
+                // choice of which exceptions to catch depends entirely on the specific task
+                // you are intending to perform and also on how much you know with certainty
+                // about the systems on which this code will run.
+                catch (UnauthorizedAccessException)
+                {
+                    continue;
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    continue;
+                }
+
+                // Push the subdirectories onto the stack for traversal.
+                // This could also be done before handing the files.
+                foreach (string str in subDirs)
+                {
+                    dirs.Push(str);
+                }
+
+                // only go through files if we have a file handler
+                if (enumFiles)
+                {
+                    IEnumerable<string> files;
+                    try
+                    {
+                        files = Directory.EnumerateFiles(currentDir);
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        continue;
+                    }
+                    catch (DirectoryNotFoundException)
+                    {
+                        continue;
+                    }
+
+                    foreach (string file in files)
+                    {
+                        yield return file;
+                    }
+                }
+            }
+        }
     }
 }
