7c029065d8ccd88ed5af6deba391963532305a06
mztikk <nevigskcuforez@gmail.com>
2019-09-17 20:06:19 +0200

    added generic FileFinder and FileSearchOptions for it

---
+++ RFReborn/Files/Finders/FileFinder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace RFReborn.Files.Finders
+{
+    public class FileFinder : IFileFinder
+    {
+        public FileSearchOptions searchOptions;
+
+        public IEnumerable<FileInfo> Find(string root)
+        {
+            foreach (string filePath in FileUtils.Walk(root, FileSystemEnumeration.FilesOnly))
+            {
+                FileInfo file;
+                try
+                {
+                    file = new FileInfo(filePath);
+                }
+                catch (FileNotFoundException)
+                {
+                    continue;
+                }
+                catch (IOException)
+                {
+                    continue;
+                }
+
+                if (searchOptions?.Compare(file) == true)
+                {
+                    yield return file;
+                }
+            }
+        }
+    }
+}
---
+++ RFReborn/Files/Finders/FileSearchOptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using RFReborn.Comparison;
+
+namespace RFReborn.Files.Finders
+{
+    public class FileSearchOptions : DynamicComparer<FileInfo>
+    {
+        public FileSearchOptions() => comparisonType = DynamicComparisonType.Any;
+
+        public string Extension { get; set; }
+
+        public string FullName { get; set; }
+
+        public bool? IsReadOnly { get; set; }
+
+        public DateTime? LastAccessTime { get; set; }
+
+        public DateTime? LastAccessTimeUtc { get; set; }
+
+        public DateTime? LastWriteTime { get; set; }
+
+        public DateTime? LastWriteTimeUtc { get; set; }
+
+        public long? Length { get; set; }
+
+        public string Name { get; set; }
+    }
+}
