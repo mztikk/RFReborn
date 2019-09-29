ed409fdf31e1db31d9e27f6db2ed550d2aad3796
mztikk <nevigskcuforez@gmail.com>
2019-09-29 20:18:16 +0200

    added more IsPrime tests

--- RFReborn.Tests/MathRTests/IsPrimeTests.cs
+++ RFReborn.Tests/MathRTests/IsPrimeTests.cs
@@ -6,9 +6,9 @@ namespace RFReborn.Tests.MathRTests
     [TestClass]
     public class IsPrimeTests
     {
-        private readonly List<int> _primes = new List<int>() { 2, 3, 5, 7, 11, 13, 17, 6701 };
+        private readonly List<int> _primes = new List<int>() { 2, 3, 5, 7, 11, 13, 17, 6701, 492876863, 982451653 };
 
-        private readonly List<int> _nonPrimes = new List<int>() { 4, 6, 8, 12, 7828 };
+        private readonly List<int> _nonPrimes = new List<int>() { 4, 6, 8, 12, 7828, 492876860 };
 
         [TestMethod]
         public void IsPrime()
