1b48e933fcd04e8b1bdbb1df5ab218652bdf38c4
mztikk <nevigskcuforez@gmail.com>
2019-09-30 21:22:12 +0200

    InternalUtils removed static constructor

--- RFReborn/InternalUtils.cs
+++ RFReborn/InternalUtils.cs
@@ -4,8 +4,6 @@ namespace RFReborn
 {
     internal static class InternalUtils
     {
-        internal static readonly CryptoRandom s_rng;
-
-        static InternalUtils() => s_rng = new CryptoRandom();
+        internal static readonly CryptoRandom s_rng = new CryptoRandom();
     }
 }
