158b64fe35b6a277c8e2c224d6473f397c81db10
mztikk <nevigskcuforez@gmail.com>
2019-10-04 19:32:31 +0200

    added ReadOnlySpanExtensions

--- RFReborn/Extensions/ReadOnlySpanExtensions.cs
+++ RFReborn/Extensions/ReadOnlySpanExtensions.cs
@@ -3,16 +3,16 @@
 namespace RFReborn.Extensions
 {
     /// <summary>
-    /// Extends <see cref="Span{T}"/>.
+    /// Extends <see cref="ReadOnlySpan{T}"/>.
     /// </summary>
-    public static class SpanExtensions
+    public static class ReadOnlySpanExtensions
     {
         /// <summary>
         /// Reverses the elements in a span of unmanaged type. 
         /// </summary>
         /// <typeparam name="T">Type</typeparam>
         /// <param name="span">Span to reverse</param>
-        public static unsafe void FastReverse<T>(this Span<T> span) where T : unmanaged
+        public static unsafe void FastReverse<T>(this ReadOnlySpan<T> span) where T : unmanaged
         {
             fixed (T* ptr = span)
             {
