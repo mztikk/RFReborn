465d16205bb47f99571d6325ae83ca5d416aa5a1
mztikk <nevigskcuforez@gmail.com>
2019-10-04 19:28:09 +0200

    added FastReverse<T>(this Span<T> array) where T : unmanaged

--- RFReborn/Extensions/SpanExtensions.cs
+++ RFReborn/Extensions/SpanExtensions.cs
@@ -7,6 +7,17 @@ namespace RFReborn.Extensions
     /// </summary>
     public static class SpanExtensions
     {
-
+        /// <summary>
+        /// Reverses the elements in an array of unmanaged type. 
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="array">Array to reverse</param>
+        public static unsafe void FastReverse<T>(this Span<T> array) where T : unmanaged
+        {
+            fixed (T* ptr = array)
+            {
+                ArrayExtensions.Reverse(ptr, array.Length);
+            }
+        }
     }
 }
