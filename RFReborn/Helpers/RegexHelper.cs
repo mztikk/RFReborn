db3d621dd1ab0cb811a80f72644858d95172de43
mztikk <nevigskcuforez@gmail.com>
2019-10-16 17:51:55 +0200

    added RegexHelper

---
+++ RFReborn/Helpers/RegexHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace RFReborn.Helpers
+{
+    /// <summary>
+    /// Helper methods for <see cref="Regex"/>
+    /// </summary>
+    public static class RegexHelper
+    {
+        /// <summary>
+        /// Escapes every string in <paramref name="input"/>
+        /// </summary>
+        /// <param name="input"><see cref="string"/>s to escape</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<string> Escape(IEnumerable<string> input)
+        {
+            foreach (string str in input)
+            {
+                yield return Regex.Escape(str);
+            }
+        }
+    }
+}
