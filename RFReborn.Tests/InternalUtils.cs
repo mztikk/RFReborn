1537d48e27ef9f3923db92aa130dd41825fed644
mztikk <nevigskcuforez@gmail.com>
2019-10-17 20:54:58 +0200

    added InternalUtils to tests

---
+++ RFReborn.Tests/InternalUtils.cs
@@ -0,0 +1,11 @@
+﻿using RFReborn.Random;
+
+namespace RFReborn.Tests
+{
+    internal static class InternalUtils
+    {
+        internal static readonly CryptoRandom s_rng = new CryptoRandom();
+
+        internal static readonly System.Random s_random = new System.Random();
+    }
+}
