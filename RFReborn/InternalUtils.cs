04dadf176e11b51b0f965cffdd5967e96e88ced2
mztikk <nevigskcuforez@gmail.com>
2019-11-09 06:22:39 +0100

    added StreamBufferSize to InternalUtils constants

--- RFReborn/InternalUtils.cs
+++ RFReborn/InternalUtils.cs
@@ -5,5 +5,6 @@ namespace RFReborn
     internal static class InternalUtils
     {
         internal static readonly CryptoRandom s_rng = new CryptoRandom();
+        internal const int StreamBufferSize = 8192;
     }
 }
