8cbc7105257e7c69689d999b96b410e29220db6a
mztikk <nevigskcuforez@gmail.com>
2019-12-19 17:46:24 +0100

    added streamextensions

---
+++ RFReborn/Extensions/StreamExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace RFReborn.Extensions
+{
+    /// <summary>
+    /// Extends <see cref="Stream"/>.
+    /// </summary>
+    public static class StreamExtensions
+    {
+        /// <summary>
+        /// Gets the remaining number of bytes in a <see cref="Stream"/> (Length - Position)
+        /// </summary>
+        /// <param name="s">Stream to use</param>
+        public static long Remaining(this Stream s) => s.Length - s.Position;
+    }
+}
