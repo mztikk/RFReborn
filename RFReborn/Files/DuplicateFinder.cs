41700e317ff6ffcbc60f2580e5c3170c602c1097
mztikk <nevigskcuforez@gmail.com>
2020-01-05 11:29:47 +0100

    added xml comments to DuplicateFinder

--- RFReborn/Files/DuplicateFinder.cs
+++ RFReborn/Files/DuplicateFinder.cs
@@ -6,18 +6,37 @@ using RFReborn.Hashing;
 
 namespace RFReborn.Files
 {
+    /// <summary>
+    /// Provides methods to find duplicate files by storing their hashes
+    /// </summary>
     public class DuplicateFinder
     {
-        public delegate string HASH(FileInfo fileInfo);
+        /// <summary>
+        /// Constructs a new <see cref="DuplicateFinder"/> with a specified hash function
+        /// </summary>
+        /// <param name="hashFunc">Hash to use</param>
+        public DuplicateFinder(Func<FileInfo, string> hashFunc) => HashFunc = hashFunc;
 
-        public DuplicateFinder(HASH hashFunc) => HashFunc = hashFunc;
-
+        /// <summary>
+        /// Constructs a new <see cref="DuplicateFinder"/> with a default hash function of xxHash
+        /// </summary>
         public DuplicateFinder() : this(XXHASH) { }
 
-        public HASH HashFunc { get; set; }
+        /// <summary>
+        /// Hash function to use
+        /// </summary>
+        public Func<FileInfo, string> HashFunc { get; set; }
 
+        /// <summary>
+        /// Finds all duplicates in given root path
+        /// </summary>
+        /// <param name="root">root path to walk</param>
         public Dictionary<string, List<FileInfo>> Find(string root) => Find(new string[] { root });
 
+        /// <summary>
+        /// Finds all duplicates in all given root paths
+        /// </summary>
+        /// <param name="rootPaths">root paths to walk</param>
         public Dictionary<string, List<FileInfo>> Find(string[] rootPaths)
         {
             Dictionary<string, FileInfo> cache = new Dictionary<string, FileInfo>();
@@ -46,8 +65,10 @@ namespace RFReborn.Files
                             cache.TryAdd(hash, fi);
                         }
                     }
+#pragma warning disable CA1031 // Do not catch general exception types
                     catch (IOException) { }
                     catch (UnauthorizedAccessException) { }
+#pragma warning restore CA1031 // Do not catch general exception types
                 });
             }
 
@@ -55,7 +76,9 @@ namespace RFReborn.Files
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#pragma warning disable IDE0051 // Remove unused private members
         private static string SHA512HASH(FileInfo input) => HashFactory.Hash("SHA512", input);
+#pragma warning restore IDE0051 // Remove unused private members
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static unsafe string XXHASH(FileInfo input)
