b3dba879d048a76100e309505b83f9324fa8e6bd
mztikk <nevigskcuforez@gmail.com>
2020-01-06 08:49:30 +0100

    added docs, fixed cooldown return value and renamed variables

--- RFReborn/Routines/RoutineBase.cs
+++ RFReborn/Routines/RoutineBase.cs
@@ -4,31 +4,42 @@ using System.Threading.Tasks;
 
 namespace RFReborn.Routines
 {
+    /// <summary>
+    /// Provides methods and base for a routinely called <see cref="Tick"/> method
+    /// </summary>
     public abstract class RoutineBase : IRoutine
     {
-        #region Base Implementations
-        private DateTime _lastCheck { get; set; }
-        private TimeSpan _refreshDelay { get; set; }
+        private DateTime _lastRun { get; set; }
+        private TimeSpan _cooldownTime { get; }
+
+        /// <summary>
+        /// Constructs a new <see cref="RoutineBase"/> with a given Cooldown Time
+        /// </summary>
+        /// <param name="cooldownTime">Cooldown time between <see cref="Tick"/> Calls</param>
+        protected RoutineBase(TimeSpan cooldownTime) => _cooldownTime = cooldownTime;
 
         /// <inheritdoc />
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public TimeSpan Cooldown()
         {
-            TimeSpan diff = DateTime.UtcNow - _lastCheck;
-            if (diff >= _refreshDelay)
+            TimeSpan diff = DateTime.UtcNow - _lastRun;
+            if (diff >= _cooldownTime)
             {
                 return TimeSpan.Zero;
             }
 
-            return diff;
+            return _cooldownTime - diff;
         }
 
+        /// <summary>
+        /// Will be called on <see cref="Tick"/> when its ready
+        /// </summary>
         protected abstract Task OnTick();
 
         /// <inheritdoc />
         public async Task Tick()
         {
-            if (Cooldown() != TimeSpan.Zero)
+            if (!Ready())
             {
                 return;
             }
@@ -39,10 +50,13 @@ namespace RFReborn.Routines
             }
             finally
             {
-                _lastCheck = ForceMinute(DateTime.UtcNow, 2, true);
+                _lastRun = ForceMinute(DateTime.UtcNow, 2, true);
             }
         }
 
+        /// <summary>
+        /// Will be called when <see cref="Kill"/> is called
+        /// </summary>
         protected abstract Task Shutdown();
 
         /// <inheritdoc />
@@ -53,6 +67,12 @@ namespace RFReborn.Routines
         public virtual bool Ready() => Cooldown() == TimeSpan.Zero;
 
         #region Utils
+        /// <summary>
+        /// Forces the lower minute on a <see cref="DateTime"/> to be on a given interval of <paramref name="min"/>
+        /// </summary>
+        /// <param name="time"><see cref="DateTime"/> to base on</param>
+        /// <param name="min">Interval to set</param>
+        /// <param name="allowSubtraction">If this is set to <see langword="true"/> it will also go back to the last higher minute instead of only rolling over</param>
         public static DateTime ForceMinute(DateTime time, int min, bool allowSubtraction = false)
         {
             if (min < 0 || min > 9)
