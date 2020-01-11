75ab9827b62d5ba217e644b8b8fb8365c2291a94
mztikk <nevigskcuforez@gmail.com>
2020-01-11 22:04:20 +0100

    added IDisposableEx

---
+++ RFReborn/Disposable/IDisposableEx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RFReborn.Disposable
+{
+    /// <summary>
+    /// Provides a mechanism for releasing unmanaged resources with additional info.
+    /// </summary>
+    public interface IDisposableEx : IDisposable
+    {
+        /// <summary>
+        /// If the object has been disposed
+        /// </summary>
+        public bool IsDisposed { get; }
+    }
+}
