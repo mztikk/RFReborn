8471b95ebbace7adfd34e5eb50d5de845d536372
mztikk <nevigskcuforez@gmail.com>
2020-03-28 13:22:27 +0100

    added IgnoreModule impl

---
+++ RFReborn/Files/FileCollector/Modules/IgnoreModule.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace RFReborn.Files.FileCollector.Modules
+{
+    /// <summary>
+    /// <see cref="ICollectingModule"/> implementation for ignore wildcard patterns
+    /// </summary>
+    public class IgnoreModule : ICollectingModule
+    {
+        private readonly IEnumerable<string> _ignorePatterns;
+
+        /// <summary>
+        /// Constructs a new <see cref="IgnoreModule"/> with given patterns
+        /// </summary>
+        /// <param name="ignorePatterns">patterns to decide if a path should be skipped or taken</param>
+        public IgnoreModule(IEnumerable<string> ignorePatterns) => _ignorePatterns = ignorePatterns;
+
+        /// <inheritdoc />
+        public bool Skip(string path) => IgnoreHelper.IsIgnored(path, _ignorePatterns);
+
+        /// <inheritdoc />
+        public bool Take(string path) => !IgnoreHelper.IsIgnored(path, _ignorePatterns);
+    }
+}
