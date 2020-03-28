90cebedd3a18fed8504a32d623574005a4107014
mztikk <nevigskcuforez@gmail.com>
2020-03-28 13:15:54 +0100

    added FileCollector and ICollectingModule to help collect files from a root path with modules deciding if it should be collected or not

---
+++ RFReborn/Files/FileCollector/FileCollector.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using RFReborn.Files.FileCollector.Modules;
+
+namespace RFReborn.Files.FileCollector
+{
+    /// <summary>
+    /// Modular class that collects files
+    /// </summary>
+    public class FileCollector
+    {
+        private readonly ICollection<ICollectingModule> _modules = new List<ICollectingModule>();
+
+        /// <summary>
+        /// Adds a new module
+        /// </summary>
+        /// <param name="module">module to add</param>
+        public FileCollector AddModule(ICollectingModule module)
+        {
+            _modules.Add(module);
+            return this;
+        }
+
+        /// <summary>
+        /// Gets all files based on <see cref="ICollectingModule"/>s
+        /// </summary>
+        /// <param name="root">Root path to travel</param>
+        /// <param name="onFile">Action to execute on every file</param>
+        public void GetFiles(string root, Action<string> onFile) => FileUtils.Walk(
+                root,
+                (string dir) =>
+                {
+                    foreach (ICollectingModule module in _modules)
+                    {
+                        if (module.Skip(dir))
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                },
+                (string file) =>
+                {
+                    foreach (ICollectingModule module in _modules)
+                    {
+                        if (module.Skip(file))
+                        {
+                            return false;
+                        }
+                    }
+
+                    onFile(file);
+                    return true;
+                }
+                );
+    }
+}
---
+++ RFReborn/Files/FileCollector/Modules/ICollectingModule.cs
@@ -0,0 +1,20 @@
+﻿namespace RFReborn.Files.FileCollector.Modules
+{
+    /// <summary>
+    /// Interface for Modules that define if a path should be collected / taken or skipped
+    /// </summary>
+    public interface ICollectingModule
+    {
+        /// <summary>
+        /// If the path should be skipped
+        /// </summary>
+        /// <param name="path">path to examine</param>
+        bool Skip(string path);
+
+        /// <summary>
+        /// If the path should be taken
+        /// </summary>
+        /// <param name="path">path to examine</param>
+        bool Take(string path);
+    }
+}
