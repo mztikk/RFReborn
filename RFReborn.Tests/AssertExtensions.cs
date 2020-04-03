7000cb8b2f59f5f3ac93efcc2de1a5db12dde2bb
mztikk <nevigskcuforez@gmail.com>
2020-04-04 00:00:13 +0200

    added AssertExtensions

---
+++ RFReborn.Tests/AssertExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RFReborn.Tests
+{
+    public static class AssertExtensions
+    {
+        public static void ThrowsExceptionMessage<T>(Action action, string exceptionMessage)
+        {
+            Type expectedType = typeof(T);
+
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                Exception val = e;
+                Type actualType = val.GetType();
+
+                Assert.AreEqual(expectedType, actualType);
+                Assert.AreEqual(exceptionMessage, val.Message);
+
+                return;
+            }
+
+            throw new AssertFailedException($"No exception thrown. Expected {expectedType.FullName}");
+        }
+    }
+}
