c0931cb9605ff014b4873cdfc54e97f0418bb44f
mztikk <nevigskcuforez@gmail.com>
2020-04-10 19:41:51 +0200

    added DictionaryExtensions ContainsKey tests

---
+++ RFReborn.Tests/DictionaryExtensionsTests/ContainsKeyTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFReborn.Extensions;
+
+namespace RFReborn.Tests.DictionaryExtensionsTests
+{
+    [TestClass]
+    public class ContainsKeyTests
+    {
+        [TestMethod]
+        public void ContainsKey()
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>()
+            {
+                ["0"] = 0,
+                ["1"] = 1,
+                ["2"] = 2,
+                ["THREE"] = 3,
+            };
+
+            Assert.IsTrue(dict.ContainsKey("1", StringComparison.Ordinal, out string _));
+            Assert.IsTrue(dict.ContainsKey("three", StringComparison.OrdinalIgnoreCase, out string key));
+            Assert.AreEqual("THREE", key);
+            Assert.IsFalse(dict.ContainsKey("three", StringComparison.Ordinal, out string _));
+            Assert.IsFalse(dict.ContainsKey("test", StringComparison.Ordinal, out string _));
+        }
+    }
+}
