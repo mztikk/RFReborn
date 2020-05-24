4dfa80c99e5e42e7d9bec9d1ac077b7f0ed7d2c8
mztikk <nevigskcuforez@gmail.com>
2020-05-24 11:44:36 +0200

    added SignatureTests

---
+++ RFReborn.Tests/AoBTests/SignatureTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFReborn.Comparison;
+using System.Collections.Generic;
+
+namespace RFReborn.Tests.AoBTests
+{
+    [TestClass]
+    public class SignatureTests
+    {
+        private readonly List<StringSignatureTest> _stringSignatureTests = new List<StringSignatureTest> {
+            new StringSignatureTest("AE FF ?? 00 FE", "xx?xx", new byte[] { 0xAE, 0xFF, 0x00, 0x00, 0xFE }),
+            new StringSignatureTest("AEFF??00FE", "xx?xx", new byte[] { 0xAE, 0xFF, 0x00, 0x00, 0xFE }),
+            new StringSignatureTest("AEF F??00F E", "xx?xx", new byte[] { 0xAE, 0xFF, 0x00, 0x00, 0xFE }),
+            new StringSignatureTest(" A E F F ? ? 0 0 F E ", "xx?xx", new byte[] { 0xAE, 0xFF, 0x00, 0x00, 0xFE }),
+            new StringSignatureTest("AE FF FF 00 FE", "xxxxx", new byte[] { 0xAE, 0xFF, 0xFF, 0x00, 0xFE }),
+            new StringSignatureTest("????????", "????", new byte[] { 0x00, 0x00, 0x00, 0x00 }),
+        };
+
+        [TestMethod]
+        public void StringSignature()
+        {
+            foreach (StringSignatureTest test in _stringSignatureTests)
+            {
+                Assert.AreEqual(test.ExpectedMask, test.Signature.Mask);
+                Assert.IsTrue(FastCompare.Equals(test.ExpectedPattern, test.Signature.Pattern));
+            }
+        }
+    }
+}
---
+++ RFReborn.Tests/AoBTests/StringSignatureTest.cs
@@ -0,0 +1,21 @@
+﻿using RFReborn.AoB;
+
+namespace RFReborn.Tests.AoBTests
+{
+    public class StringSignatureTest
+    {
+        public string Sig { get; }
+        public string ExpectedMask { get; }
+        public byte[] ExpectedPattern { get; }
+        public Signature Signature { get; }
+
+        public StringSignatureTest(string sig, string expectedMask, byte[] expectedPattern)
+        {
+            Sig = sig;
+            ExpectedMask = expectedMask;
+            ExpectedPattern = expectedPattern;
+
+            Signature = new Signature(sig);
+        }
+    }
+}
