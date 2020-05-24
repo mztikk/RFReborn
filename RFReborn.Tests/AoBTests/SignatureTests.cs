2491ff2ae62e83dfa9f47006e2d28f9a6cae01d4
mztikk <nevigskcuforez@gmail.com>
2020-05-24 14:46:39 +0200

    added Signature Tests for halfbyte masking

--- RFReborn.Tests/AoBTests/SignatureTests.cs
+++ RFReborn.Tests/AoBTests/SignatureTests.cs
@@ -9,6 +9,8 @@ namespace RFReborn.Tests.AoBTests
     {
         private readonly List<StringSignatureTest> _stringSignatureTests = new List<StringSignatureTest> {
             new StringSignatureTest("AE FF ?? 00 FE", "xx?xx", new byte[] { 0xAE, 0xFF, 0x00, 0x00, 0xFE }),
+            new StringSignatureTest("AE FF ?F 00 FE", "xx?xx", new byte[] { 0xAE, 0xFF, 0x00, 0x00, 0xFE }),
+            new StringSignatureTest("AE FF F? 00 FE", "xx?xx", new byte[] { 0xAE, 0xFF, 0x00, 0x00, 0xFE }),
             new StringSignatureTest("AEFF??00FE", "xx?xx", new byte[] { 0xAE, 0xFF, 0x00, 0x00, 0xFE }),
             new StringSignatureTest("AEF F??00F E", "xx?xx", new byte[] { 0xAE, 0xFF, 0x00, 0x00, 0xFE }),
             new StringSignatureTest(" A E F F ? ? 0 0 F E ", "xx?xx", new byte[] { 0xAE, 0xFF, 0x00, 0x00, 0xFE }),
