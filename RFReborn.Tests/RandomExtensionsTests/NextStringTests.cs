5626166c14872a34a4af11155638bf45a9d782e6
mztikk <nevigskcuforez@gmail.com>
2020-06-20 20:01:04 +0200

    added RandomExtensions.NextString tests

---
+++ RFReborn.Tests/RandomExtensionsTests/NextStringTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFReborn.Extensions;
+using RFReborn.RandomR;
+
+namespace RFReborn.Tests.RandomExtensionsTests
+{
+    [TestClass]
+    public class NextStringTests
+    {
+        [DataTestMethod]
+        [DataRow("ABCDEF", 8)]
+        [DataRow("ABCDEF", 20)]
+        public void ContainsOnlyCharset(string charset, int len)
+        {
+            Random rnd = new CryptoRandom();
+            foreach (char c in rnd.NextString(charset, len))
+            {
+                Assert.IsTrue(charset.Contains(c));
+            }
+        }
+    }
+}
