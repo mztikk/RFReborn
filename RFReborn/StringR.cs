5e84d5c67d3e10f36956660664b4e9a137e908e2
mztikk <nevigskcuforez@gmail.com>
2020-07-04 11:13:04 +0200

    StringR.SplitN ArgumentNullException message

--- RFReborn/StringR.cs
+++ RFReborn/StringR.cs
@@ -154,7 +154,7 @@ namespace RFReborn
         {
             if (str is null)
             {
-                throw new ArgumentNullException(nameof(str) + " can't be null.");
+                throw new ArgumentNullException(nameof(str));
             }
 
             if (n <= 0)
