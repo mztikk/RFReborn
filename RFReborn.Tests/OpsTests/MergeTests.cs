428035dcfad90fa7928fe5c3749f2630c6c9bc87
mztikk <nevigskcuforez@gmail.com>
2020-07-04 11:38:43 +0200

    added MergeTests

---
+++ RFReborn.Tests/OpsTests/MergeTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFReborn.Comparison;
+
+namespace RFReborn.Tests.OpsTests
+{
+    [TestClass]
+    public class MergeTests
+    {
+        [TestMethod]
+        public void Merge()
+        {
+            Assert.IsTrue(FastCompare.Equals(new[] { 1, 2, 3, 4 }, Ops.Merge(new[] { 1 }, new[] { 2, 3, 4, }).ToArray()));
+            Assert.IsTrue(FastCompare.Equals(new[] { 1, 2, 3, 4 }, Ops.Merge(new[] { 1, 2 }, new[] { 3, 4, }).ToArray()));
+            Assert.IsTrue(FastCompare.Equals(new[] { 1, 2, 3, 4 }, Ops.Merge(new[] { 1 }, new[] { 2 }, new[] { 3 }, new[] { 4 }).ToArray()));
+        }
+    }
+}
