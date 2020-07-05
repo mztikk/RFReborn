5d2f969158bb1024af370f05f842894e9bece488
mztikk <nevigskcuforez@gmail.com>
2020-07-05 21:25:27 +0200

    added SegmentCopy

---
+++ RFReborn/Copying/SplitFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace RFReborn.Copying
+{
+    /// <summary>
+    /// Class that wraps a <see cref="FileInfo"/> and splits it into segments, each segment being the size of <see cref="SplitSize"/>
+    /// </summary>
+    internal class SplitFile
+    {
+        public SplitFile(FileInfo file, long splitSize)
+        {
+            File = file;
+            SplitSize = splitSize;
+            SplitInfos = GetSplitInfos(file, splitSize);
+        }
+
+        public FileInfo File { get; }
+        public long SplitSize { get; }
+        public SplitInfo[] SplitInfos { get; }
+
+        private static SplitInfo[] GetSplitInfos(FileInfo file, long splitSize)
+        {
+            long l = file.Length;
+            double t = l / (double)splitSize;
+            int n = (int)Math.Ceiling((double)t);
+
+            var rtn = new SplitInfo[n];
+            int i = 0;
+            while (l > 0)
+            {
+                rtn[i++] = new SplitInfo(file.Length - l, (int)Math.Min(l, splitSize));
+
+                l -= splitSize;
+            }
+
+            return rtn;
+        }
+    }
+}
---
+++ RFReborn/Copying/SplitInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace RFReborn.Copying
+{
+    /// <summary>
+    /// Class that represents a split part with a <see cref="StartIndex"/>, <see cref="Length"/> and <see cref="EndIndex"/>
+    /// </summary>
+    internal class SplitInfo
+    {
+        public SplitInfo(long startIndex, int length)
+        {
+            StartIndex = startIndex;
+            Length = length;
+            EndIndex = startIndex + length;
+        }
+
+        public long StartIndex { get; }
+        public int Length { get; }
+        public long EndIndex { get; }
+    }
+}
