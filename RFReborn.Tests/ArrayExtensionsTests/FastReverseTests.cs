3ca15e542d4b6afba292bb2716290aede12e45c1
mztikk <nevigskcuforez@gmail.com>
2020-08-27 21:59:14 +0200

    added FastReverseTests

---
+++ RFReborn.Tests/ArrayExtensionsTests/FastReverseTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFReborn.Extensions;
+
+namespace RFReborn.Tests.ArrayExtensionsTests
+{
+    [TestClass]
+    public class FastReverseTests
+    {
+        [TestMethod]
+        public void FastReverse()
+        {
+            int[] testArray = new[] { 1, 2, 3, 4, 5 };
+            int[] testArrayReverse = new[] { 5, 4, 3, 2, 1 };
+
+            CollectionAssert.AreNotEqual(testArray, testArrayReverse);
+            testArray.FastReverse();
+            CollectionAssert.AreEqual(testArray, testArrayReverse);
+        }
+    }
+}
