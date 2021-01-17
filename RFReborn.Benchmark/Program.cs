0044f77a21a936787108b4cb93f399ae2773815b
mztikk <nevigskcuforez@gmail.com>
2021-01-17 10:27:22 +0100

    added benchmark project and aob scan benchmark

---
+++ RFReborn.Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace RFReborn.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<AoBScannerBenchmark>();
+        }
+    }
+}
