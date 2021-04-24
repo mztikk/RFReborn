b0aea43e43d150cc2f6b77fc0fd8fafb5ba872df
Nils Feierabend <mztikk@outlook.de>
2021-04-25 01:13:20 +0200

    added DynamicProperty Get tests

---
+++ RFReborn.Tests/DynamicPropertyTests/GetTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFReborn.Internals;
+
+namespace RFReborn.Tests.DynamicPropertyTests
+{
+    [TestClass]
+    public class GetTests
+    {
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(50)]
+        public void GetIntDirect(int n)
+        {
+            var test = new { N = n };
+            Assert.AreEqual(n, DynamicProperty.Get(test, "N"));
+        }
+
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(50)]
+        public void GetChainedInt(int n)
+        {
+            var test = new { inner = new { N = n } };
+            Assert.AreEqual(n, DynamicProperty.Get(test.GetType(), test, new[] { "inner", "N" }));
+        }
+    }
+}
