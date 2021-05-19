a03d84a6c6943a4c855bdb2d6bd773ba1ce13e52
Nils Feierabend <mztikk@outlook.de>
2021-05-19 17:15:29 +0200

    added DisposableAction tests

---
+++ RFReborn.Tests/DisposableTests/DisposableActionTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFReborn.Disposable;
+
+namespace RFReborn.Tests.DisposableTests
+{
+    [TestClass]
+    public class DisposableActionTests
+    {
+        [TestMethod]
+        public void ActionIsCalled()
+        {
+            bool called = false;
+            void callMe() => called = true;
+            using (_ = new DisposableAction(callMe)) { }
+
+            Assert.IsTrue(called);
+        }
+
+        [TestMethod]
+        public void ActionIsCalledOnlyOnce()
+        {
+            int calls = 0;
+            void callMe() => calls++;
+            var disposable = new DisposableAction(callMe);
+            disposable.Dispose();
+            disposable.Dispose();
+            disposable.Dispose();
+            disposable.Dispose();
+            disposable.Dispose();
+            disposable.Dispose();
+            disposable.Dispose();
+
+            Assert.AreEqual(1, calls);
+        }
+    }
+}
