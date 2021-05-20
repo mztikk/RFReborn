69ff0c18aba492272b6bd54e6f76a91610ec4191
Nils Feierabend <mztikk@outlook.de>
2021-05-20 22:03:28 +0200

    added IEnumerableExtensionsTests for Any and Count

---
+++ RFReborn.Tests/IEnumerableExtensionsTests/AnyTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFReborn.Extensions;
+
+namespace RFReborn.Tests.IEnumerableExtensionsTests
+{
+    [TestClass]
+    public class AnyTests
+    {
+        [TestMethod]
+        public void OneItemInArrayIsTrue()
+        {
+            int[] test = new int[] { 1 };
+            Assert.IsTrue(test.Any());
+        }
+
+        [TestMethod]
+        public void OneItemInIEnumerableIsTrue()
+        {
+            static IEnumerable<int> test()
+            {
+                yield return 0;
+            }
+
+            Assert.IsTrue(test().Any());
+        }
+
+        [TestMethod]
+        public void ItemInIEnumerableMatchesSelector()
+        {
+            static IEnumerable<int> test()
+            {
+                yield return 0;
+            }
+
+            Assert.IsTrue(test().Any(x => x == 0));
+        }
+
+        [TestMethod]
+        public void ItemInIEnumerableDoesntMatchSelector()
+        {
+            static IEnumerable<int> test()
+            {
+                yield return 0;
+            }
+
+            Assert.IsFalse(test().Any(x => x == 1));
+        }
+
+        [TestMethod]
+        public void EmptyArrayIsFalse() => Assert.IsFalse(Array.Empty<bool>().Any());
+
+        [TestMethod]
+        public void EmptyIEnumerableIsFalse()
+        {
+            static IEnumerable<int> test()
+            {
+                yield break;
+            }
+
+            Assert.IsFalse(test().Any());
+        }
+    }
+}
---
+++ RFReborn.Tests/IEnumerableExtensionsTests/CountTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFReborn.Extensions;
+
+namespace RFReborn.Tests.IEnumerableExtensionsTests
+{
+    [TestClass]
+    public class CountTests
+    {
+        [TestMethod]
+        public void OneItemInArray()
+        {
+            int[] test = new int[] { 1 };
+            Assert.AreEqual(1, test.Count());
+        }
+
+        [TestMethod]
+        public void EmptyArray() => Assert.AreEqual(0, Array.Empty<int>().Count());
+
+        [TestMethod]
+        public void OneItemInIEnumerable()
+        {
+            static IEnumerable<int> test()
+            {
+                yield return 1;
+            }
+            Assert.AreEqual(1, test().Count());
+        }
+
+        [TestMethod]
+        public void EmptyIEnumerable()
+        {
+            static IEnumerable<int> test()
+            {
+                yield break;
+            }
+            Assert.AreEqual(0, test().Count());
+        }
+    }
+}
